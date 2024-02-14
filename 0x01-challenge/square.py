5088132641b8cc2f2c731aa350d79b84c087001e
Imrane Arabah <59772524+arabahimrane@users.noreply.github.com>
2024-02-14 13:19:27 -0500

    0x00e

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Class module for square application """
+
+
+class Square():
+    """ square for printing and calclulating square """
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """ init stuff of function """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def permiter_of_my_square(self):
+        """ Permiter Of the Square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ Print the with and height of aquare """
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    """ Exemple of my square """
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
